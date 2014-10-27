b6a1b0b575559b2029fb60f9bb32a3d290c95bc8
Kina Smith <kina.smith@gmail.com>
2014-10-27 16:31:22 -0400

    combined the node_testing/server.js with pi_node_testing/server.js to include gpio connection

--- node_testing/server.js
+++ node_testing/server.js
@@ -1,4 +1,8 @@
-var express = require('express');
+var express = require('express'),
+    Gpio = require('onoff').Gpio,
+    led = new Gpio(17, 'out'),
+    button = new Gpio(27, 'in', 'both');
+
 var app = express();
 app.listen(8080);
 
@@ -7,6 +11,18 @@ var foodBoolean = 1, // 0: food == false, 1: food == true
     foodAmount = 4; // map(food amount, small, great, 0, 4)
 
 console.log("server started @ 8080");
+console.log("please press button");
+
+function sendData(request) {
+    request.respond(latestData);
+}
+
+button.watch(function(err, value) {
+    if (err) throw err;
+    console.log(value);
+    led.writeSync(value);
+    foodBoolean = value;
+});
 
 app.get('/', function(request, response) {
     response.sendFile(__dirname + '/public/index.html')
--- pi_node_testing/server.js
+++ pi_node_testing/server.js
@@ -1,27 +1,42 @@
-var servi = require("servi"),
-  Gpio = require('onoff').Gpio, 
-  led = new Gpio(17, 'out'), 
-  button = new Gpio(27, 'in', 'both');
+var express = require('express'),
+    Gpio = require('onoff').Gpio,
+    led = new Gpio(17, 'out'),
+    button = new Gpio(27, 'in', 'both');
 
-var latestData = 0;
+var app = express();
+app.listen(8080);
 
-var app = new servi(false);
-app.port(8080);
-app.serveFiles("public");
-app.route('/data', sendData);
-app.start();
+var foodBoolean = 1, // 0: food == false, 1: food == true
+    foodType = 2, // 0: neutral, 1: sweet, 2: savory
+    foodAmount = 4; // map(food amount, small, great, 0, 4)
 
-console.log("server is running");
+console.log("server started @ 8080");
 console.log("please press button");
 
-function sendData(request) {   
- request.respond(latestData);
+function sendData(request) {
+    request.respond(latestData);
 }
 
 button.watch(function(err, value) {
-  if(err) throw err;
-  console.log(value);
-  led.writeSync(value);
-  latestData = value;
+    if (err) throw err;
+    console.log(value);
+    led.writeSync(value);
+    foodBoolean = value;
 });
 
+app.get('/', function(request, response) {
+    response.sendFile(__dirname + '/public/index.html')
+    console.log("sent index.html");
+});
+
+app.get('/food', function(request, response) {
+    response.sendStatus(foodBoolean);
+});
+
+app.get('/type', function(request, response) {
+    response.sendStatus(foodType);
+});
+
+app.get('/amount', function(request, response) {
+    response.sendStatus(foodAmount);
+});
