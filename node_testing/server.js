9e597235e75c2975fafdd3f6eda612cde50b41ce
Kina Smith <kina.smith@gmail.com>
2014-10-29 13:47:15 -0400

    can now send info via restian interface

--- node_testing/server.js
+++ node_testing/server.js
@@ -1,12 +1,12 @@
-var express = require('express'),
-    Gpio = require('onoff').Gpio,
-    led = new Gpio(17, 'out'),
-    button = new Gpio(27, 'in', 'both');
+var express = require('express'); //,
+//  Gpio = require('onoff').Gpio,
+//  led = new Gpio(17, 'out'),
+//  button = new Gpio(27, 'in', 'both');
 
 var app = express();
 app.listen(8080);
 
-var foodBoolean = 1, // 0: food == false, 1: food == true
+var foodState = 1, // 0: food == false, 1: food == true
     foodType = 2, // 0: neutral, 1: sweet, 2: savory
     foodAmount = 4; // map(food amount, small, great, 0, 4)
 
@@ -16,27 +16,54 @@ console.log("please press button");
 function sendData(request) {
     request.respond(latestData);
 }
-
+/*
 button.watch(function(err, value) {
     if (err) throw err;
     console.log(value);
     led.writeSync(value);
     foodBoolean = value;
 });
-
-app.get('/', function(request, response) {
+*/
+app.get('/', function(req, res) {
     response.sendFile(__dirname + '/public/index.html')
     console.log("sent index.html");
 });
 
-app.get('/food', function(request, response) {
-    response.sendStatus(foodBoolean);
+app.get('/food', function(req, res) {
+    response.send({
+        state: foodState
+    });
+});
+app.get('/food/:state', function(req, res) {
+    foodState = req.params.state;
+    sendState(res);
 });
 
-app.get('/type', function(request, response) {
-    response.sendStatus(foodType);
+app.get('/type', function(req, res) {
+    response.send({
+        type: foodType
+    });
+});
+app.get('/type/:type', function(req, res) {
+    foodType = req.params.type;
+    sendState(res);
 });
 
-app.get('/amount', function(request, response) {
-    response.sendStatus(foodAmount);
+app.get('/amount', function(req, res) {
+    response.send({
+        amount: foodAmount
+    });
+
 });
+app.get('/amount/:amount', function(req, res) {
+    foodAmount = req.params.amount;
+    sendState(res);
+});
+
+function sendState(data) {
+    data.send({
+        state: foodState,
+        type: foodType,
+        amount: foodAmount
+    });
+}
