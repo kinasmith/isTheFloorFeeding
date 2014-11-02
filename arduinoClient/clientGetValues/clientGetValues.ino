748b22a763ff18e4c338fcecf090a9ee7e4850f5
Kina Smith <kina.smith@gmail.com>
2014-11-02 14:41:20 -0500

    got the display sketch mostly finalized with the enclosure built

--- arduinoClient/clientGetValues/clientGetValues.ino
+++ arduinoClient/clientGetValues/clientGetValues.ino
@@ -24,6 +24,15 @@ boolean lastConnected = false;                 // state of the connection last t
 const unsigned long postingInterval = 5*1000;  // delay between updates, in milliseconds
 
 //create pin assignments
+int statePin = 3;
+int sweetPin = 2;
+int savoryPin = 4;
+int amount1Pin = 5;
+int amount2Pin = 6;
+int amount3Pin = 7;
+int amount4Pin = 8;
+int netStatusLEDG = 0;
+int netStatusLEDR = 1;
 
 //create data holders
 int state = 0;
@@ -35,24 +44,37 @@ String response = "";
 bool begin = false;
 
 void setup() {
-  pinMode(9, OUTPUT);
-  digitalWrite(9, LOW);
+  for(int i = 0; i <= 9; i++) {
+    pinMode(i, OUTPUT);
+    digitalWrite(i, LOW); 
+  }
+
   // start serial port:
-  Serial.begin(9600);
+  // Serial.begin(9600);
 
   // start the Ethernet connection:
   if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");
+    // Serial.println("Failed to configure Ethernet using DHCP");
     // no point in carrying on, so do nothing forevermore:
     // try to congifure using IP address instead of DHCP:
     Ethernet.begin(mac, ip);
+
+    digitalWrite(netStatusLEDR, HIGH);
+    digitalWrite(netStatusLEDG, LOW);
+  } 
+  else {
+    digitalWrite(netStatusLEDR, LOW);
+    digitalWrite(netStatusLEDG, HIGH);
   }
   delay(1000); //wait for shield to initialize
-  Serial.println("connecting...");
+  // Serial.println("connecting...");
 }
 
 void loop() {
-  digitalWrite(9, state);
+  setState(state);
+  setType(type);
+  setAmount(amount);
+
   // if there's incoming data from the net connection.
   // send it out the serial port.  This is for debugging
   // purposes only:
@@ -65,22 +87,25 @@ void loop() {
   if(client.available() || !begin) {
     char in = client.read();
     if (in == '{') {
+      digitalWrite(netStatusLEDG, HIGH);
       begin = true;
     }
     if (begin) response += (in);
     if (in == '}') {
       begin = false;
       parseJSON(response);
-      Serial.print(response); 
+      // Serial.print(response); 
       response = "";
+      digitalWrite(netStatusLEDG, LOW);
     }
+
   }
 
   // if there's no net connection, but there was one last time
   // through the loop, then stop the client:
   if (!client.connected() && lastConnected) {
-    Serial.println();
-    Serial.println("disconnecting.");
+    // Serial.println();
+    // Serial.println("disconnecting.");
     client.stop();
   }
 
@@ -100,7 +125,7 @@ void httpRequest() {
   String HTTPtype = " HTTP/1.1\r\n"; 
   // if there's a successful connection:
   if (client.connect(server, 80)) {
-    Serial.println("connecting...");
+    //  Serial.println("connecting...");
     //send the data to the server using GET requests. 
     //heroku didn't like println's so I'm using just .print commands
     //send three GET requests, then close connection
@@ -113,23 +138,27 @@ void httpRequest() {
   } 
   else {
     // if you couldn't make a connection:
-    Serial.println("connection failed");
-    Serial.println("disconnecting.");
+    //  Serial.println("connection failed");
+    // Serial.println("disconnecting.");
     client.stop();
+
+    digitalWrite(netStatusLEDG, LOW);
+    digitalWrite(netStatusLEDR, HIGH);
   }
 }
 
 void parseJSON(String json) {
   char charBuf[json.length()+1];
   json.toCharArray(charBuf, json.length()+1);
-  
+
   JsonParser<16> parser;
   JsonObject root = parser.parse(charBuf); 
 
   if (!root.success())
   {
-    Serial.println("JsonParser.parse() failed");
+    //  Serial.println("JsonParser.parse() failed");
     return;
+    digitalWrite(netStatusLEDR, HIGH);
   }
 
   long _state  = root["state"];
@@ -138,12 +167,67 @@ void parseJSON(String json) {
   state = _state;
   type = _type;
   amount = _amount;
-  Serial.println(_state);
-  Serial.println(_type);
-  Serial.println(_amount);
-
-
+  // Serial.println(_state);
+  // Serial.println(_type);
+  // Serial.println(_amount);
 }
+
+
+void setState(int input) {
+  digitalWrite(statePin, input);
+}
+
+void setType(int input) {
+  switch(input) {
+  case 1:
+    digitalWrite(savoryPin, HIGH);
+    digitalWrite(sweetPin, LOW);
+    break;
+  case 2:
+    digitalWrite(savoryPin, LOW);
+    digitalWrite(sweetPin, HIGH);
+    break;
+  default:
+    digitalWrite(savoryPin, LOW);
+    digitalWrite(sweetPin, LOW);
+  }
+}
+
+void setAmount(int input) {
+  for(int i = amount1Pin; i <= amount4Pin; i++){
+    if(input > (i-amount1Pin)) {
+      digitalWrite(i, HIGH);
+    } 
+    else {
+      digitalWrite(i, LOW);
+    }
+  } 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
